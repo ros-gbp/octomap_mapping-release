c3d7ab5ac6ce39f8117e1f6b900c98a964799b4b
Armin Hornung <armin@hornung.io>
2016-03-25 20:52:59 +0100

    Rebase from 'debian/jade/octomap_server'

--- src/octomap_server_node.cpp
+++ src/octomap_server_node.cpp
@@ -40,33 +40,41 @@
 #include <octomap_server/OctomapServer.h>
 
 #define USAGE "\nUSAGE: octomap_server <map.[bt|ot]>\n" \
-		"  map.bt: inital octomap 3D map file to read\n"
+        "  map.bt: inital octomap 3D map file to read\n"
 
 using namespace octomap_server;
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "octomap_server");
-  std::string mapFilename("");
+  const ros::NodeHandle& private_nh = ros::NodeHandle("~");
+  std::string mapFilename(""), mapFilenameParam("");
 
   if (argc > 2 || (argc == 2 && std::string(argv[1]) == "-h")){
     ROS_ERROR("%s", USAGE);
     exit(-1);
   }
 
-
   OctomapServer server;
   ros::spinOnce();
 
   if (argc == 2){
     mapFilename = std::string(argv[1]);
+  }
+
+  if (private_nh.getParam("map_file", mapFilenameParam)) {
+    if (mapFilename != "") {
+      ROS_WARN("map_file is specified by the argument '%s' and rosparam '%s'. now loads '%s'", mapFilename.c_str(), mapFilenameParam.c_str(), mapFilename.c_str());
+    } else {
+      mapFilename = mapFilenameParam;
+    }
+  }
+
+  if (mapFilename != "") {
     if (!server.openFile(mapFilename)){
       ROS_ERROR("Could not open file %s", mapFilename.c_str());
       exit(1);
     }
   }
-
-
-
 
   try{
     ros::spin();
